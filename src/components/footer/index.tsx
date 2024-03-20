85792d9a4bc10bc008ad98bb8960bf1fa25fb0c6
Harshil <sondagarharshil0@gmail.com>
2024-03-20 20:42:27 +0530

    done

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -227,7 +227,7 @@ export default function Footer() {
         <hr className="mt-16 mb-10 border-gray-200" />
 
         <p className="text-sm text-center text-gray-600">
-          © Copyright 2021, All Rights Reserved by Postcraft
+          © Copyright 2021, All Rights Reserved by swiftbiz
         </p>
       </div>
     </section>
