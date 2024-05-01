95271e9c1db737ced00c6fed2bc849170946bcd1
Harshil <sondagarharshil0.gmail.com>
2024-05-02 01:59:49 +0530

    @

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -6,17 +6,8 @@ import Section from "../landing_component/features";
 import Steps from "../landing_component/steps";
 import Testimonial from "../landing_component/testimonials";
 import Videos from "../landing_component/videos";
-import { data } from "../../constant/features";
-import csvDownload from "json-to-csv-export";
-import Button from "../common/button";
 
 export default function LeadingPage() {
-  const dataToConvert: any = {
-    data: data.data,
-    filename: "data",
-    delimiter: ",",
-    headers: ["id", "img", "title", "Description"]
-  };
   return (
     <>
       <Header isLanding={true} />
